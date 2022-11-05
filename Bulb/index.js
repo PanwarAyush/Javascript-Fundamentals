90aabd744b1a525bc44108eaf4ea1faffe2e2c2d
Ayush Panwar <ayushpanwar691@gmail.com>
2022-11-05 17:49:58 +0530

    initial commit

---
+++ Bulb/index.js
@@ -0,0 +1,6 @@
+const turnOffLight=()=>{
+ document.getElementById('bulb').src="bulboff.png"
+}
+const turnOnLight=()=>{
+ document.getElementById('bulb').src="bulb on.png"
+}
---
+++ showDate/index.js
@@ -0,0 +1,6 @@
+const showTime=()=>{
+document.getElementById('time').innerHTML=Date()
+}
+const hideTime=()=>{
+ document.getElementById('time').innerHTML=""
+}
